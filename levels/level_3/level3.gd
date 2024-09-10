00358542670370101a562c5729b5603857e3d775
bruno.lopes <95176651+koddoki@users.noreply.github.com>
2024-09-09 21:45:10 -0300

    Level 3 completed wip level 4

--- levels/level_3/level3.gd
+++ levels/level_3/level3.gd
@@ -3,6 +3,7 @@ extends Level
 
 @onready var dialogue_scene := $DialogueScene
 @onready var background := $Background
+@onready var animation := $AnimationPlayer
 
 var json_count = 1
 var scenarios : Array
@@ -16,6 +17,7 @@ func _ready():
 
 
 func next_scene():
+	print(scenarios.size())
 	if scenarios.size() > 0:
 		var scenario = scenarios.pop_back()
 		var background_path = scenario["background"] if scenario.has("background") else null
@@ -27,19 +29,61 @@ func next_scene():
 		dialogue_scene.update_characters(character_data)
 		if background_path != null:
 			background.texture = load(background_path)
+		return
 	match json_count:
 		1:
 			var json_as_text = FileAccess.get_file_as_string("res://levels/level_3/jsons/FASE3P2.json")
 			scenarios = JSON.parse_string(json_as_text)
 			json_count += 1
 			
-			dialogue_scene.visible = true
 			next_scene()
+			return
 		2:
-			pass
-	
+			var json_as_text = FileAccess.get_file_as_string("res://levels/level_3/jsons/FASE3P3.json")
+			scenarios = JSON.parse_string(json_as_text)
+			json_count += 1
+			
+			dialogue_scene.visible = false
+			animation.play("folhas_ruins")
+			return
+		3:
+			var json_as_text = FileAccess.get_file_as_string("res://levels/level_3/jsons/FASE3P4.json")
+			scenarios = JSON.parse_string(json_as_text)
+			json_count += 1
+			
+			dialogue_scene.visible = false
+			animation.play("folhas_boas")
+			return
+		4:
+			var json_as_text = FileAccess.get_file_as_string("res://levels/level_3/jsons/FASE3P5.json")
+			scenarios = JSON.parse_string(json_as_text)
+			json_count += 1
+			next_scene()
+			return
+		5:
+			var json_as_text = FileAccess.get_file_as_string("res://levels/level_3/jsons/FASE3P6.json")
+			scenarios = JSON.parse_string(json_as_text)
+			json_count += 1
+			next_scene()
+			return
+		6:
+			end.emit()
+			return
+	print("Erro fudeu")
 	emit_signal("end")
 
 
 func _on_dialogue_scene_next_button_pressed():
+	next_scene()
+
+
+func _on_negar_pressed():
+	dialogue_scene.visible = true
+	$FolhasRuins.queue_free()
+	next_scene()
+
+
+func _on_aprovar_pressed():
+	dialogue_scene.visible = true
+	$FolhasBoas.queue_free()
 	next_scene()
--- levels/level_4/level_4.gd
+++ levels/level_4/level_4.gd
@@ -2,32 +2,91 @@ extends Level
 
 
 @onready var dialogue_scene := $DialogueScene
+@onready var background := $Background
+@onready var animation := $AnimationPlayer
 
-
-var dialogues : Array
+var json_count = 1
+var scenarios : Array
 
 
 func _ready():
 	var json_as_text = FileAccess.get_file_as_string("res://levels/level_4/jsons/FASE4P1.json")
 	dialogue_scene.visible = true
-	dialogues = JSON.parse_string(json_as_text)
+	scenarios = JSON.parse_string(json_as_text)
 	next_scene()
 
 
 func next_scene():
-	if dialogues.size() > 0:
-		var dialogue = dialogues.pop_back()
-		var background = dialogue["background"] if dialogue.has("background") else null
-		var title = dialogue["title"] if dialogue.has("title") else null
-		var text = dialogue["text"] if dialogue.has("text") else null
-		var character_data = dialogue["character_data"] if dialogue.has("character_data") else null
+	print(scenarios.size())
+	if scenarios.size() > 0:
+		var scenario = scenarios.pop_back()
+		var background_path = scenario["background"] if scenario.has("background") else null
+		var title = scenario["title"] if scenario.has("title") else null
+		var text = scenario["text"] if scenario.has("text") else null
+		var character_data = scenario["character_data"] if scenario.has("character_data") else null
+		
 		dialogue_scene.update_text(title, text)
 		dialogue_scene.update_characters(character_data)
-		dialogue_scene.update_background(background)
+		if background_path != null:
+			background.texture = load(background_path)
 		return
-	
+	match json_count:
+		1:
+			var json_as_text = FileAccess.get_file_as_string("res://levels/level_3/jsons/FASE3P2.json")
+			scenarios = JSON.parse_string(json_as_text)
+			json_count += 1
+			
+			next_scene()
+			return
+		2:
+			var json_as_text = FileAccess.get_file_as_string("res://levels/level_3/jsons/FASE3P3.json")
+			scenarios = JSON.parse_string(json_as_text)
+			json_count += 1
+			
+			dialogue_scene.visible = false
+			animation.play("folhas_ruins")
+			return
+		3:
+			var json_as_text = FileAccess.get_file_as_string("res://levels/level_3/jsons/FASE3P4.json")
+			scenarios = JSON.parse_string(json_as_text)
+			json_count += 1
+			
+			dialogue_scene.visible = false
+			animation.play("folhas_boas")
+			return
+		4:
+			var json_as_text = FileAccess.get_file_as_string("res://levels/level_3/jsons/FASE3P5.json")
+			scenarios = JSON.parse_string(json_as_text)
+			json_count += 1
+			next_scene()
+			return
+		5:
+			var json_as_text = FileAccess.get_file_as_string("res://levels/level_3/jsons/FASE3P6.json")
+			scenarios = JSON.parse_string(json_as_text)
+			json_count += 1
+			next_scene()
+			return
+		6:
+			end.emit()
+	print("Erro fudeu")
 	emit_signal("end")
 
 
 func _on_dialogue_scene_next_button_pressed():
 	next_scene()
+
+
+func _on_negar_pressed():
+	dialogue_scene.visible = true
+	$FolhasRuins.queue_free()
+	next_scene()
+
+
+func _on_aprovar_pressed():
+	dialogue_scene.visible = true
+	$FolhasBoas.queue_free()
+	next_scene()
+
+
+func _on_texture_button_pressed():
+	print("botao")
